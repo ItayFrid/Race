35b4502f6ac4310629e0c76212a23eda90679f7c
Itay Fridman <fitay123@gmail.com>
2018-06-05 15:42:40 +0300

    Add TODOs

---
+++ EX4/src/TODO.java
@@ -0,0 +1,11 @@
+
+public class TODO {
+/**
+ * TODO Remove Wheeld,Color fields from Racer
+ * TODO Add IRacer Interface
+ * TODO make Racer Class Implements IRacer
+ * TODO Add RacerDecorator Class Implements IRacer
+ * TODO Add ColoredRacer,WheeledRacer Classes Inherit from RacerDecorator
+ * TODO Update addRacer() in landArena to be able to add WheeledRacer
+ */
+}
