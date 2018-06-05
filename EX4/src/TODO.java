2c16660e480529f676b25e8cc2f4f280ecc4ec95
Itay Fridman <fitay123@gmail.com>
2018-06-05 16:23:50 +0300

    TODOs

--- EX4/src/TODO.java
+++ EX4/src/TODO.java
@@ -1,6 +1,7 @@
 
 public class TODO {
 /**
+ * TODO Remove todoClass
  * TODO Remove Wheeled,Color fields from Racer
  * TODO Update addRacer() in landArena to be able to add WheeledRacer
  * TODO Update Racer::Introduce() to print from Hashtable
