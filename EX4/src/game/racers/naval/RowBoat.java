4d7dfb7cb6392ef482ce005914445c92b49abf8c
Itay Fridman <fitay123@gmail.com>
2021-10-14 10:02:16 +0300

    Update RowBoat.java

--- EX4/src/game/racers/naval/RowBoat.java
+++ EX4/src/game/racers/naval/RowBoat.java
@@ -7,7 +7,6 @@ import utilities.EnumContainer.Team;
 /**
  * This is the RowBoat racer class
  * @author Idan Aharon, Itay Fridman
- * 			 305437774 305360653
  * @see Racer, NavalRacer
  */
 public class RowBoat extends Racer implements NavalRacer {
